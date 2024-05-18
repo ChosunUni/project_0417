adaa29559310e6b743c8b721aded6ab87718fc62
limch2320 <limch2320@naver.com>
2024-05-19 00:00:24 +0900

    total_funtion 05_19

--- src/main/java/com/suntime/study/entity/TimerEntity.java
+++ src/main/java/com/suntime/study/entity/TimerEntity.java
@@ -29,6 +29,20 @@ public class TimerEntity {
     @Column
     private Integer seconds;
 
+    public void addTime(int hours, int minutes, int seconds) {
+        this.hours += hours;
+        this.minutes += minutes;
+        this.seconds += seconds;
+
+        // 초가 60을 넘으면 분으로 넘김
+        this.minutes += this.seconds / 60;
+        this.seconds %= 60;
+
+        // 분이 60을 넘으면 시간으로 넘김
+        this.hours += this.minutes / 60;
+        this.minutes %= 60;
+    }
+
     public static TimerEntity toTimerEntity(TimerDTO timerDTO){
         TimerEntity timerEntity = new TimerEntity();
         timerEntity.setEmail(timerDTO.getEmail());
